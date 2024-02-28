e06e33b948b6e0444869350546bcefefedca2346
Kirill Biktimirov <kirillbiktya@users.noreply.github.com>
2024-02-28 20:18:44 +0300

    Добавил выборочный обход папок

---
+++ README.md
@@ -0,0 +1,8 @@
+# Mycego тестовое задание
+
+Использование:
+- Первый запуск для получения токена:
+`python main.py`
+- Проход по всем папкам:
+`python main.py --token <TOKEN> --url <PUBLIC_FOLDER_URL>`
+- Выборочное использование папок: `python main.py --token <TOKEN> --url <PUBLIC_FOLDER_URL> --folder <SUBFOLDER_NAME> [--folder <SUBFOLDER_NAME> ...]`
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import argparse
 import yadisk
 import image_concatenator as imc
-from PIL import Image , ImageFile
-ImageFile.LOAD_TRUNCATED_IMAGES = True
+from PIL import Image
 import requests
 from io import BytesIO
 
@@ -10,7 +9,8 @@ CLIENT_ID = '10b72e30a2814393866862e946f06832'
 
 parser = argparse.ArgumentParser(prog='Mycego тестовое', description='Скачивает изображения с Яндекс.Диска, собирая их в один файл TIFF')
 parser.add_argument('--token', help='Токен для авторизации в API Яндекс.Диска. Если не указан - будет выведена ссылка на авторизацию')
-parser.add_argument('--url', help='URL публичной папки, в которой содержатся данные', required=True)
+parser.add_argument('--url', help='URL публичной папки, в которой содержатся данные')
+parser.add_argument('-f', '--folder', help='Папка, которую использовать для создания изображения. Может использоваться несколько раз. Если не указывать - программа пройдет по всем папкам', action='append')
 args = parser.parse_args()
 
 if args.token is None:
@@ -28,6 +28,9 @@ with client:
     if client.public_exists(args.url):
         items = client.public_listdir(args.url)
         for item in items:
+            if args.folder is not None:
+                if item.name not in args.folder:
+                    continue
             print(item.name)
             files = item.public_listdir(path=item.path + '/', limit=20)
             images = []
